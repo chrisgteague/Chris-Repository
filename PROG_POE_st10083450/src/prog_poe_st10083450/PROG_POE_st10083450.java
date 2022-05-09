c76ec2bb3e92dc60d90f8053d34949b886021967
chrisgteague <st10083450@vcconnect.edu.za>
2022-05-09 16:58:07 +0200

    update

--- PROG_POE_st10083450/src/prog_poe_st10083450/PROG_POE_st10083450.java
+++ PROG_POE_st10083450/src/prog_poe_st10083450/PROG_POE_st10083450.java
@@ -5,11 +5,12 @@ import javax.swing.*;
 public class PROG_POE_st10083450
 {
 
-    private Login login;
+    
 
     public static void main(String[] args)
     {
-        Login input = new login(System.in);
+    
+        
     }
 
 }
